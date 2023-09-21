bfd21d9340d2265a6ee3b52de8803c3b94f45c84
Isaac Mitchell <tmitchellisaac@gmail.com>
2023-09-21 15:34:03 -0600

    Modify plant conditional

--- plants.rb
+++ plants.rb
@@ -1,9 +1,11 @@
 
-gets_sunshine = true
-ounces_of_water = 11
+gets_sunshine = false
+ounces_of_water = 9
 
 if gets_sunshine == true && ounces_of_water >= 10
     puts "Plant is thriving!"
+elsif gets_sunshine == true || ounces_of_water >= 10
+    puts "Plant is fine,  but not thriving"
 else 
-    puts "Plant is not thriving :("
+    puts "Plant is not thriving"
 end
